c3878a3155f0e5cdf7504645bb4e1cbf9066aa54
Andrés Felipe <camachorodriguezandresfelipe@gmail.com>
2026-05-13 18:36:27 -0500

    watsapp

--- src/components/Ws/Watsapp.jsx
+++ src/components/Ws/Watsapp.jsx
@@ -7,7 +7,7 @@ import '../Ws/Ws.css';
 function Watsapp() {
   return (
     <div className="whatsapp-flotante">
-      <a href="https://wa.me/3238733372" target="_blank" rel="noopener noreferrer">
+      <a href="https://wa.me/573238733372" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon className="whatsapp-icono-img" icon={faWhatsapp} size="3x" color="#25D366" />
       </a>
     </div>
