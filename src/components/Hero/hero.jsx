e1a723f63fec9319882a941397a16aaa000923d1
Andrés Felipe <camachorodriguezandresfelipe@gmail.com>
2026-05-15 10:46:37 -0500

    cambios tipografia e infraestructura y texto

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -4,22 +4,22 @@ export default function Hero() {
   return (
     <section className="hero" id="inicio">
       <div className="hero-content">
-        <h1 >
-          Código que crea. 
-        </h1>
 
         <h1>
-          <span className="texto">Automatización real.</span>
+          Código que escala.{" "}
+          <br />
+          <span className="texto">Negocios que crecen.</span>
         </h1>
 
-        <p>
-          Desarrollamos apps y software a medida con automatizaciones reales e IA funcional.
+        <p className="subtitulo">
+          Construimos tecnología, marcas y estrategias para negocios ambiciosos.
         </p>
 
         <div className="buttons">
           <button>Ver proyectos</button>
           <button className="outline">Contactar</button>
         </div>
+
       </div>
 
       <div className="hero-glow"></div>
