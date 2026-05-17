41881929c56cfbc85b3c38584da181bf0e50eaea
Andrés Felipe <camachorodriguezandresfelipe@gmail.com>
2026-05-16 22:03:42 -0500

    cambios seguridad

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [ react() ],
-  base: 'https://development-one1.github.io/hexjia/',
+  plugins: [react()],
+
+  base: '/hexjia/',
+
+  build: {
+    sourcemap: false,   // ❌ evita ver código original
+    minify: 'terser',   // 🔥 hace el JS más difícil de leer
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      mangle: true,
+    }
+  },
+
   optimizeDeps: {
-    include: [ '@fontawesome/fontawesome-svg-core' ]
+    include: ['@fontawesome/fontawesome-svg-core']
   }
 })
