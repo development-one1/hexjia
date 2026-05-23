95d0f376d05f1c254cb93b4ccbb88ff81f641c72
Andrés Felipe <camachorodriguezandresfelipe@gmail.com>
2026-05-22 19:18:37 -0500

    navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,28 +1,30 @@
 import "../Navbar/navbar.css";
-
 import icono from "../../assets/icono.png";
 
 export default function Navbar() {
   return (
     <header className="navbar">
-      <div className="logo">
+      
+      <a href="#inicio" className="logo" aria-label="Ir al inicio">
         <img 
           src={icono} 
-          alt="HEXJIA logo" 
+          alt="HEXJIA logo"
           className="hex"
           draggable="false"
-          onDragStart={(e) => e.preventDefault()}
-           /> 
+        />
+        <span className="logo-text">
           <span className="exx">HEX</span>
           <span className="jia">JIA</span>
-      </div>
+        </span>
+      </a>
 
-      <nav>
-        <a href="#inicio">Inicio</a>
+      <nav className="nav-links">
         <a href="#servicios">Servicios</a>
+        <a href="#certifications">Certificados</a>
         <a href="#proyectos">Proyectos</a>
         <a href="#contacto">Contacto</a>
       </nav>
+
     </header>
   );
 }
