c59da7c58d73c39246fef405b749cf5d61fb77d4
Andres Felipe Camacho <camachorodriguezandresfelipe@gmail.com>
2026-09-16 17:23:09 -0500

    cambios anuncio

---
+++ src/escala-main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Escala from "./pages/Escala.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Escala />
+  </React.StrictMode>
+);
